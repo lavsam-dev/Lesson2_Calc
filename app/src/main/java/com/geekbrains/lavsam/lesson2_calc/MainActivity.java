2e1770544aa650aeed94ee3252150d8778567119
lavsam-dev <lavsam@gmail.com>
2021-06-01 23:05:01 +0400

    Lesson3 practice init

--- app/src/main/java/com/geekbrains/lavsam/lesson2_calc/MainActivity.java
+++ app/src/main/java/com/geekbrains/lavsam/lesson2_calc/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView screenMessage;
+    private CalcPilum calc = new CalcPilum();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,53 +27,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         LayoutInflater inflater = getLayoutInflater();
         FrameLayout l = (FrameLayout) inflater.inflate(R.layout.activity_main, null);
 
-        ViewGroup Current_Widget = (ViewGroup)l.getRootView();
-        int childCount = Current_Widget.getChildCount();
-        Log.d("viewgroup", "Current_Widget.getChildCount() " + childCount);
-
-        for (int i = 0; i < Current_Widget.getChildCount(); i++) {
-            View currentWidget = (View) Current_Widget.getChildAt(i);
-            Log.d("viewgroup", "currentWidget.getTag() " + currentWidget.getTag());
-//            if ((String)currentWidget.getTag() == "top_grid") {
-                ViewGroup Current_W = (ViewGroup) Current_Widget.getChildAt(i);
-            Log.d("viewgroup", "currentWidget.getTag() " + currentWidget.getTag());
-                Log.d("viewgroup", "Current_W.getChildCount() " + Current_W.getChildCount());
-                for (int j = 0; j < Current_W.getChildCount(); j++) {
-                    View currentButton = (View) Current_W.getChildAt(i);
-                    if (currentButton.getTag() == "button") {
-                        findViewById(currentButton.getId()).setOnClickListener(this);
-                    }
-                }
-//            }
-
-        }
-
-//        findViewById(R.id.btn_1).setOnClickListener(this);
-//        findViewById(R.id.btn_2).setOnClickListener(this);
-//        findViewById(R.id.btn_3).setOnClickListener(this);
-//        findViewById(R.id.btn_4).setOnClickListener(this);
-//        findViewById(R.id.btn_5).setOnClickListener(this);
-//        findViewById(R.id.btn_6).setOnClickListener(this);
-//        findViewById(R.id.btn_7).setOnClickListener(this);
-//        findViewById(R.id.btn_8).setOnClickListener(this);
-//        findViewById(R.id.btn_9).setOnClickListener(this);
-//        findViewById(R.id.btn_0).setOnClickListener(this);
-//        findViewById(R.id.btn_clear).setOnClickListener(this);
-//        findViewById(R.id.btn_clearAll).setOnClickListener(this);
-//        findViewById(R.id.btn_div).setOnClickListener(this);
-//        findViewById(R.id.btn_mult).setOnClickListener(this);
-//        findViewById(R.id.btn_plus).setOnClickListener(this);
-//        findViewById(R.id.btn_minus).setOnClickListener(this);
-//        findViewById(R.id.btn_dot).setOnClickListener(this);
-//        findViewById(R.id.btn_eq).setOnClickListener(this);
+        findViewById(R.id.btn_1).setOnClickListener(this);
+        findViewById(R.id.btn_2).setOnClickListener(this);
+        findViewById(R.id.btn_3).setOnClickListener(this);
+        findViewById(R.id.btn_4).setOnClickListener(this);
+        findViewById(R.id.btn_5).setOnClickListener(this);
+        findViewById(R.id.btn_6).setOnClickListener(this);
+        findViewById(R.id.btn_7).setOnClickListener(this);
+        findViewById(R.id.btn_8).setOnClickListener(this);
+        findViewById(R.id.btn_9).setOnClickListener(this);
+        findViewById(R.id.btn_0).setOnClickListener(this);
+        findViewById(R.id.btn_clear).setOnClickListener(this);
+        findViewById(R.id.btn_clearAll).setOnClickListener(this);
+        findViewById(R.id.btn_div).setOnClickListener(this);
+        findViewById(R.id.btn_mult).setOnClickListener(this);
+        findViewById(R.id.btn_plus).setOnClickListener(this);
+        findViewById(R.id.btn_minus).setOnClickListener(this);
+        findViewById(R.id.btn_dot).setOnClickListener(this);
+        findViewById(R.id.btn_eq).setOnClickListener(this);
 
     }
 
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_1) screenMessage.setText(R.string.key_1);
-        if (v.getId() == R.id.btn_2) screenMessage.setText(R.string.key_2);
+    public void onClick(View v)  {
+        if (v.getId() == R.id.btn_1) screenMessage.setText(screenMessage.getText() + "1");
+        if (v.getId() == R.id.btn_2) screenMessage.setText(screenMessage.getText() + "2");
         if (v.getId() == R.id.btn_3) screenMessage.setText(R.string.key_3);
         if (v.getId() == R.id.btn_4) screenMessage.setText(R.string.key_4);
         if (v.getId() == R.id.btn_5) screenMessage.setText(R.string.key_5);
@@ -85,9 +65,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.btn_clearAll) screenMessage.setText(R.string.key_clearAll);
         if (v.getId() == R.id.btn_div) screenMessage.setText(R.string.key_div);
         if (v.getId() == R.id.btn_mult) screenMessage.setText(R.string.key_mult);
-        if (v.getId() == R.id.btn_plus) screenMessage.setText(R.string.key_plus);
+        if (v.getId() == R.id.btn_plus) screenMessage.setText(screenMessage.getText() + "+");
         if (v.getId() == R.id.btn_minus) screenMessage.setText(R.string.key_minus);
         if (v.getId() == R.id.btn_dot) screenMessage.setText(R.string.key_dot);
-        if (v.getId() == R.id.btn_eq) screenMessage.setText(R.string.key_eq);
+        if (v.getId() == R.id.btn_eq) {
+//            screenMessage.setText(R.string.key_eq);
+            double result = calc.Calculate((String)screenMessage.getText());
+            screenMessage.setText(screenMessage.getText() + "\n" + result);
+//            try {
+//                double result = calc.Calculate((String)screenMessage.getText());
+//                screenMessage.setText(screenMessage.getText() + "\n=" + result);
+//            } catch (NumberFormatException) {
+//                screenMessage.setText("ERROR");
+//            }
+        }
     }
 }
